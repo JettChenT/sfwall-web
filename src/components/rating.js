20a183dcf30c7c3ec74abbed41e686da43bd2faa
JettChenT <jettchen12345@gmail.com>
2021-07-04 23:46:02 +0800

    Add number slider

--- src/components/rating.js
+++ src/components/rating.js
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import ClipLoader from "react-spinners/ClipLoader";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+const marks = {
+  0: "Hideous",
+  1: "Breathtaking",
+  0.5: "Neutral",
+};
 
 const Rater = () => {
   const [imageid, setImageid] = useState("");
@@ -9,7 +17,8 @@ const Rater = () => {
   const { getAccessTokenSilently } = useAuth0();
   let [loading, setLoading] = useState(true);
   let [started, setStarted] = useState(false);
-  let [msg,setMsg] = useState("Press Start to start ranking images!")
+  let [rating, setRating] = useState(0.5);
+  let [msg, setMsg] = useState("Press Start to start ranking images!");
 
   const UpdateImgId = async () => {
     try {
@@ -41,11 +50,12 @@ const Rater = () => {
         photo_id: imageid,
       };
       UpdateImgId();
+      setRating(0.5);
       axios
         .post(`${serverUrl}/rate`, data, { headers: headers })
         .then((res) => {
           console.log(res.data);
-        //   setLoading(false);
+          //   setLoading(false);
         });
     } catch (error) {
       alert(error.message);
@@ -53,49 +63,64 @@ const Rater = () => {
   };
 
   const start = async () => {
-      await UpdateImgId();
-      setStarted(true);
-      setMsg("Loading image...")
+    await UpdateImgId();
+    setStarted(true);
+    setMsg("Loading image...");
     //   setLoading(false)
-  }
+  };
 
-  const load = async ()=>{
-    if(started){
-        setLoading(false)
+  const load = async () => {
+    if (started) {
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <div className="flex flex-col h-screen my-auto items-center">
       <img
         className="w-3/5"
         src={`https://source.unsplash.com/${imageid}/1600x900`}
-        style={loading?{display:'none'}:{}}
-        onLoad={()=>load()}
+        style={loading ? { display: "none" } : {}}
+        className="object-contain h-2/3 w-full"
+        onLoad={() => load()}
       ></img>
-      <span>{loading&&msg}</span>
+      <span>{loading && msg}</span>
       <ClipLoader loading={loading} size={150} />
       {
-    started ?
-      <div className="inline-flex">
-        <button
-          className="bg-blue-300 hover:bg-blue-400 text-gray-800 font-bold py-2 px-4 rounded-l"
-          onClick={() => makerank(1)}
-        >
-          Like
-        </button>
-        <button
-          className="bg-red-300 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded-r"
-          onClick={() => makerank(0)}
-        >
-          Dislike
-        </button>
-      </div> : <button
-          className="bg-red-300 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded"
-          onClick={() => start()}>
-              START
+        started && 
+        <Slider
+          min={0}
+          max={1}
+          step={0.1}
+          startPoint={0.5}
+          dots={true}
+          marks={marks}
+          value={rating}
+          className="max-w-md m-5 mb-10"
+          onChange={(value) => setRating(value)}
+          disabled={!started||loading}
+        />
+      }
+
+      {started ? (
+        <div className="flex">
+          <button
+            className="bg-blue-300 hover:bg-blue-500 disabled:opacity-50 transform hover:scale-105  text-gray-800 font-bold py-2 px-4 rounded"
+            onClick={() => makerank(rating)}
+            disabled={!started||loading}
+          >
+            RATE
           </button>
-    }
+          <br />
+        </div>
+      ) : (
+        <button
+          className="bg-red-300 hover:bg-red-500 text-gray-800 font-bold py-2 px-4 rounded"
+          onClick={() => start()}
+        >
+          START
+        </button>
+      )}
     </div>
   );
 };
