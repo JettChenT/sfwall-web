5f548141b997f0d0d1860b0e1126ec7fc219d30b
JettChenT <jettchen12345@gmail.com>
2021-07-11 10:51:59 +0800

    Solve purgeCSS issue

--- src/components/funlink.js
+++ src/components/funlink.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+/**
+   * PurgeCSS:
+   * from-red-500
+   * from-green-500
+   * from-yellow-500
+   * from-gray-500
+   * from-purple-500
+   * from-indigo-500
+   * from-blue-500
+   * from-pink-500
+   * to-red-500
+   * to-green-500
+   * to-yellow-500
+   * to-gray-500
+   * to-purple-500
+   * to-indigo-500
+   * to-blue-500
+   * to-pink-500
+   * bg-red-900
+   * bg-green-900
+   * bg-yellow-900
+   * bg-gray-900
+   * bg-purple-900
+   * bg-indigo-900
+   * bg-blue-900
+   * bg-pink-900
+*/
+
 const FunLink = ( {title, description, to, linkDesc, fcolor="purple", tcolor="indigo"} ) => {
     return (
         <div className={`bg-gradient-to-br from-${fcolor}-500 to-${tcolor}-500 p-8 text-white rounded-3xl mt-10 mr-10 w-auto transition duration-200 ease-in-out transform hover:-translate-y-1 flex-1`}>
